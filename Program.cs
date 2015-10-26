7c1f46f002f8d7bf802f32b7a60eba85456109e0
hamdiceylan <hamdiceylan@gmail.com>
2015-10-26 14:05:46 +0200

    First Console Sample

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.Write("İlk Sayıyı Giriniz :");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("İkinci Sayıyı Giriniz :");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girdiğiniz sayıların toplamı : "+ (sayi1+sayi2).ToString());
+            Console.ReadLine();
+        }
+    }
+}
